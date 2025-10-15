a29b8c6817a22fcd2254c2f094e61cc088014cfc
Vishal Raj Rawani <jimmy91648@gmail.com>
2025-10-15 19:47:21 +0530

    api went

--- Index.js
+++ Index.js
@@ -3,6 +3,19 @@ const express = require('express')
 
 const app = express();
 
+const github1 = {
+   
+        "userId": 1,
+        "id": 1,
+        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+      
+}
+
+app.get('/github',(req,res)=>{
+    res.json(github1);
+})
+
 app.get('/',(req,res)=>{
     res.send("gand mara chutiye");
 })
